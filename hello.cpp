e21c1b32dba006efca2f1c62d7393af83910c69a
Armen <armenak-chobanyan@mail.ru>
2019-09-23 23:56:27 +0400

    this is my change

--- hello.cpp
+++ hello.cpp
@@ -1,6 +1,10 @@
 #include<iostream>
+#include<string>
 
 int main() {
-    std::cout<<"Hello word"<<std::endl;
+    std::cout << "whot is your name "<<std::flush;
+    std::string name;
+    std::cin >> name;
+    std::cout << "Hello " << name << std::endl;
     return 0;
 }
