977414777418e43d2a6c9dd9320c82a73dd88e8f
Armen <armenak-chobanyan@mail.ru>
2019-09-24 20:29:16 +0400

    wrong words

--- hello.cpp
+++ hello.cpp
@@ -2,7 +2,7 @@
 #include<string>
 
 int main() {
-    std::cout << "whot is your name "<<std::flush;
+    std::cout << "what is your name "<<std::flush;
     std::string name;
     std::cin >> name;
     std::cout << "Hello " << name << std::endl;
